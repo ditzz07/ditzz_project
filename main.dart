0878f716adf6e64d56beaf7aed113501caeb3afb
Aditya R.F <danggerblack123@gmail.com>
2024-10-18 10:01:20 +0700

    versi 1.1

--- main.dart
+++ main.dart
@@ -1,64 +1,80 @@
-//memasukkan package yang dibuituhkan aplikasi
-import 'package:english_words/english_words.dart'; 
-//paket bahasa inggris
-import 'package:flutter/material.dart'; 
-//paket untuk tampilan UI (material)
-import 'package:provider/provider.dart';
-//paket untuk interaksi aplikasi
+//memasukkan package yang dibutuhkan oleh aplikasi
+import 'package:english_words/english_words.dart'; //paket bahasa inggris
+import 'package:flutter/material.dart'; //paket untuk tampilan UI (material UI)
+import 'package:provider/provider.dart'; //paket untuk interaksi aplikasi
 
+//fungsi main (fungsi utama)
 void main() {
-  runApp(MyApp());
+  runApp(
+      MyApp()); //memanggil fungsi runApp (yg menjalankan keseluruhan aplikasi di dalam MyApp())
+  //Karena MyApp adalah fungsi aplikasi mobile, maka haru dipanggil dari fungsi runApp()
 }
 
-//membuat abstrak aplikasi dari StatelessWidget (template aplikasi), aplikasi nya bernama MyApp
+//membuat abstrak aplikasi dari statelessWidget (template aplikasi), aplikasinya bernama MyApp
 class MyApp extends StatelessWidget {
-  const MyApp({super.key}); //menunjukkan bahwa aplikasi ini akan tetap,tidak berubah setelah di build
+  const MyApp(
+      {super.key}); //menunjukkan bahwa aplikasi ini akan tetap, tidak berubah setelah di-build
 
-  @override //mengganti nilai nama yang sudah ada di template,dengan nilai nilai yang baru (replace / overwrite)
+  @override //mengganti nilai lama yg sudah ada di template, dengan nilai-nilai yg baru (replace / overwrite)
   Widget build(BuildContext context) {
-    //funsi build adalah fungsi yg membangun UI (mengatur posisi widget, dst)
-    //ChangeNotifierProfider mendengarkan/mendektesi semua interaksi yg terjadi di aplikasi
+    //fungsi build adalah fungsi yg membangun UI (mengatur posisi widget, dst)
+    //ChangeNotifierProvider mendengarkan/mendeteksi semua interaksi yang terjadi di aplikasi
     return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp( //pada state ini, menggunakan style desain MaterialUI
-        title: 'Namer App', // diberi judul Namer App
-        theme: ThemeData( // data tema aplikasi, diberi warna deepOrange
-          useMaterial3: true,
+      create: (context) => MyAppState(), //membuat satu state bernama MyAppState
+      child: MaterialApp(
+        //pada state ini, menggunakan style desain MaterialUI
+        title: 'Namer App', //diberi judul Namer App
+        theme: ThemeData(
+          //data tema aplikasi, diberi warna deepOrange
+          useMaterial3: true, //versi materialUI yang dipakai versi 3
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         ),
-        home: MyHomePage(), //nama halaman "MyHomePage" yang menggunakan states 'MyHomePage'
+        home:
+            MyHomePage(), //nama halaman "MyHomePage" yang menggunakan state "MyAppState".
       ),
     );
   }
 }
-//mendefinisikan myAppState
+
+//mendefinisikan isi MyAppState
 class MyAppState extends ChangeNotifier {
-  //state MyAppState diisi dengan 2 kata random yang di gabung.Kata random tsb diismpan dalam variable Wordpair
+  //state MyAppState diisi dengan 2 kata random yang digabung. Kata random tsb disimpan di variable WordPair
   var current = WordPair.random();
+
+  //membuat fungsi getNext untuk mengacak kata
+  void getNext() {
+    current = WordPair.random(); //acak kata
+    notifyListeners(); //kirim kata yg diacak ke listener untuk ditampilkan di layar
+  }
 }
 
-// ...
-
+//membuat layout pada halaman HomePage
 class MyHomePage extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {           // ← 1
-    var appState = context.watch<MyAppState>();  // ← 2
-
-    return Scaffold(                             // ← 3
-      body: Column(                              // ← 4
+  Widget build(BuildContext context) {
+    var appState =
+        context.watch<MyAppState>(); //widget menggunakan state MyAppState
+    //di bawah ini adalah kode program untuk menyusun layout
+    return Scaffold(
+      //base (canvas) dari layout
+      body: Column(
+        //di atas scaffold, ada body. Body-nya, diberi kolom
         children: [
-          Text('A random AWESOME idea:'),        // ← 5
-          Text(appState.current.asLowerCase),    // ← 6
+          //di dalam kolom, diberi teks
+          Text('A random idea:'),
+          Text(appState.current
+              .asLowerCase), //mengambil random text dari appState pada variabel WordPair current, lalu diubah menjadi huruf kecil semua, dan ditampilkan sebagai teks
           ElevatedButton(
+            //membuat button timbul di dalam body
             onPressed: () {
-              print('button pressed!');
+              //fungsi getNext() dieksekusi ketika button ditekan
+              appState.getNext();
             },
-            child: Text('Next'),
+            child:
+                Text('Next'), //berikan teks 'Next' pada button (sebagai child)
           ),
-        ],                                       // ← 7
+        ],
       ),
     );
   }
 }
-
-// ...
