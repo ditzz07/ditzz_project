9cecc7cebd5418d11d040b71b89cb8c692b7d72e
Aditya R.F <danggerblack123@gmail.com>
2024-10-25 09:25:38 +0700

    versi 1.3
    menambahkan kolom samping

--- main.dart
+++ main.dart
@@ -1,36 +1,41 @@
 //memasukkan package yang dibutuhkan oleh aplikasi
-import 'package:english_words/english_words.dart';//paket bahasa inggris
-import 'package:flutter/material.dart';//paket untuk tampilan UI (material UI)
-import 'package:provider/provider.dart';//paket untuk interaksi aplikasi
+import 'package:english_words/english_words.dart'; //paket bahasa inggris
+import 'package:flutter/material.dart'; //paket untuk tampilan UI (material UI)
+import 'package:provider/provider.dart'; //paket untuk interaksi aplikasi
 
 //fungsi main (fungsi utama)
 void main() {
-  runApp(MyApp());//memanggil fungsi runApp (yg menjalankan keseluruhan aplikasi di dalam MyApp)
+  runApp(
+      MyApp()); //memanggil fungsi runApp (yg menjalankan keseluruhan aplikasi di dalam MyApp)
 }
-
 
 //membuat abstrak aplikasi dari statelessWidget (template aplikasi), aplikasi bernama MyApp
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});//menunjukkan bahwa aplikasi ini akan tetap, tidak berubah setelah di-build
+  const MyApp(
+      {super.key}); //menunjukkan bahwa aplikasi ini akan tetap, tidak berubah setelah di-build
 
-  @override //mengganti nilai lama yg sudah ada di template, dengan nilai-nilai yg baru (replace / overwrite) 
+  @override //mengganti nilai lama yg sudah ada di template, dengan nilai-nilai yg baru (replace / overwrite)
   Widget build(BuildContext context) {
     //fungsi yg membagun UI (mengatur posisi widget, dst)
     //ChangeNotifierProvider / mendeteksi semua interaksi yang terjadi di aplikasi
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       //membuat satu state bernama MyAppState
-      child: MaterialApp( // pada state ini, menggunakan style desain MaterialUI
+      child: MaterialApp(
+        // pada state ini, menggunakan style desain MaterialUI
         title: 'Namer App', //diberi judul Namer App
-        theme: ThemeData( //data tema aplikasi, diberi warna deepOrange
+        theme: ThemeData(
+          //data tema aplikasi, diberi warna deepOrange
           useMaterial3: true, //versi materialUI yang dipakai versi 3
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue.shade200),
         ),
-        home: MyHomePage(), //nama halaman "MyHomePage" yang menggunakan state "MyAppState".
+        home:
+            MyHomePage(), //nama halaman "MyHomePage" yang menggunakan state "MyAppState".
       ),
     );
   }
 }
+
 //mendefinisikan isi MyAppState
 class MyAppState extends ChangeNotifier {
   //state MyAppState diisi dengan 2 kata random yang digabung. kata random tsb disimpan di variable WordPair
@@ -54,12 +59,59 @@ class MyAppState extends ChangeNotifier {
 }
 
 //membuat layout pada halaman HomePage
-class MyHomePage extends StatelessWidget {
+// ...
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          SafeArea(
+            child: NavigationRail(
+              extended: false,
+              destinations: [
+                NavigationRailDestination(
+                  icon: Icon(Icons.home),
+                  label: Text('Home'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.favorite),
+                  label: Text('Favorites'),
+                ),
+              ],
+              selectedIndex: selectedIndex,
+              onDestinationSelected: (value) {
+                setState(() {
+                  selectedIndex = value;
+                });
+              },
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: GeneratorPage(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
-    var pair = appState.current;//variable pair menyimpan kata yang sedang tampil/aktif
-    //di bawah ini adalah kode program untuk menyusut layout
+    var pair = appState.current;
 
     IconData icon;
     if (appState.favorites.contains(pair)) {
@@ -68,41 +120,38 @@ class MyHomePage extends StatelessWidget {
       icon = Icons.favorite_border;
     }
 
-    return Scaffold( //base (canvas) dari layout
-      body: Center(
-        child: Column( //di atas scaffold, ada body. Body-nya, diberi kolom
+    return Center(
+      child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-          children: [ //di dalam kolom, diberi teks
-            BigCard(pair: pair), //mengambil nilai dari variable pair, lalu diubah menjadi huruf kecil semua, dan ditampilkan sebagai teks
-            SizedBox(height: 10,),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                
-                ElevatedButton.icon(
-                  onPressed: () {
-                    appState.toggleFavorite();
-                  },
-                  icon: Icon(icon),
-                  label: Text('Like'),
-                ),
-                SizedBox(width: 10),
-
-                ElevatedButton( //membuat button timbul di dalam body
-                  onPressed: () { //fungsi yang dieksekusi ketika button di tekan 
-                    appState.getNext(); //tampilan teks 'button pressed' di terminal saat button di tekan 
-                  },
-                  child: Text('Next'), //berikan text 'Next' pada button (sebagai child)
-                ),
-              ],
-            ),
-          ],
-        ),
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
 }
 
+// ...
 class BigCard extends StatelessWidget {
   const BigCard({
     super.key,
@@ -121,8 +170,13 @@ class BigCard extends StatelessWidget {
     return Card(
       color: theme.colorScheme.primary,
       child: Padding(
-        padding: const EdgeInsets.all(20),//memberi jarak/padding di sekitar teks
-        child: Text(pair.asLowerCase, style: style,semanticsLabel: "${pair.first} ${pair.second}",),//membuat kata mencajdi huruf kecil
+        padding:
+            const EdgeInsets.all(20), //memberi jarak/padding di sekitar teks
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ), //membuat kata mencajdi huruf kecil
       ),
     );
   }
